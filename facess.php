1fd41b7ea50d4233311d2bac7309915c94fc3b44
RsaLMBR <114023061+RsaLMBR@users.noreply.github.com>
2022-10-28 18:36:21 -0300

    fds

--- facess.php
+++ facess.php
@@ -4,9 +4,9 @@
 <?php hint("will include the arg specified in the GET parameter \"page\""); ?>
 
 <form action="/LFI-1/index.php" method="GET">
-    <input type="text" name="paged">
+    <input type="text" name="page">
 </form>
 
 <?php
-include($_GET["paged"]);
+include($_GET["page"]);
 ?>
