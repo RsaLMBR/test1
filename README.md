77f2bc97a59ec34b45060dab1956fa5dad01ae26
RsaLMBR <114023061+RsaLMBR@users.noreply.github.com>
2022-10-28 15:29:55 -0300

    test2 dependa

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # test1
 Testing Codacy
+I'am RSA
--- facess.php
+++ facess.php
@@ -4,9 +4,9 @@
 <?php hint("will include the arg specified in the GET parameter \"page\""); ?>
 
 <form action="/LFI-1/index.php" method="GET">
-    <input type="text" name="page">
+    <input type="text" name="paged">
 </form>
 
 <?php
-include($_GET["page"]);
+include($_GET["paged"]);
 ?>
