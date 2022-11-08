8c09af26986c75965821a4e237d2422137c397c7
Rannyere Andrade <114023061+RsaLMBR@users.noreply.github.com>
2022-11-08 00:14:53 -0300

    test_action

--- sqlinjection.php
+++ sqlinjection.php
@@ -17,8 +17,8 @@ if (strlen($_GET['id']) < 1) {
   $count = $db->querySingle('select count(*) from secrets where id = ' . $_GET['id']);
 
   if ($count > 0) {
-    echo 'Sim!';
+    echo 'Yeah!';
   } else {
-    echo 'Não!';
+    echo 'Oh no!';
   }
 }
