1334209c507338ca26ce4d1df380ebc4e3cadb0b
Rannyere Andrade <114023061+RsaLMBR@users.noreply.github.com>
2022-12-19 16:37:49 -0300

    alt1

--- fileinclusion.php
+++ fileinclusion.php
@@ -2,12 +2,12 @@
 
 <!-- from http://www.ush.it/2009/02/08/php-filesystem-attack-vectors/ -->
 
-<?php hint("will include the arg specified in the POST parameter \"class\", appends .php to end, defeat with NULL byte %00"); ?>
+<?php hint("will include the arg specified in the POST parameter \"clas\", appends .php to end, defeat with NULL byte %00"); ?>
 
 <form action="/LFI-9/index.php" method="POST">
-    <input type="text" name="class">
+    <input type="text" name="clas">
 </form>
 
 <?php
-include('includes/class_'.addslashes($_POST['class']).'.php');
+include('includes/class_'.addslashes($_POST['clas']).'.php');
 ?>
